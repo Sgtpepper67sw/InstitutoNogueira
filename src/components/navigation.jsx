579c69206d807c22340184b57dd323f1795978f3
Sgtpepper67sw <willianfxdxemc2@gmail.com>
2023-11-06 15:24:34 -0300

    cabeçalho

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,9 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            <div id="rodape">
-               <img src="img/Instituto.jfif/800/400"/> 
-            </div>
+            
           </a>{" "}
         </div>
 
